9f0238a450db7aee64041b4b34d29c7c25bac2e6
Sergey Mikheev <sergeypolosh@gmail.com>
2023-04-12 11:32:56 +0600

    feat: app now runs on local server

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import uvicorn
+from src.core.settings import settings
+
+
+def main():
+    uvicorn.run(
+        'server:app',
+        reload=True,
+        host=settings.host,
+        port=settings.port,
+    )
+
+
 if __name__ == '__main__':
-    pass
+    main()
